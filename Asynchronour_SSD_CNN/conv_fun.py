8e4d8cf704a7065a6a415f38924b2a2e6c29c7aa
Dola Ram <42748067+dolaram@users.noreply.github.com>
2018-12-10 18:34:19 +0530

    Update conv_fun.py

--- Asynchronour_SSD_CNN/conv_fun.py
+++ Asynchronour_SSD_CNN/conv_fun.py
@@ -20,27 +20,23 @@ def createNetwork():
 
     # input layer
     s = tf.placeholder("float", [None, 80, 80, 4])
-
+    
     # hidden layers
     h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
 
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
 
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
-
-    # readout layer
+    
     readout = tf.matmul(h_fc1, W_fc2) + b_fc2
 
     return s, readout, h_fc1
-    
+
 def weight_variable(shape):
     initial = tf.random_normal(shape, stddev = 0.01)
     return tf.Variable(initial)
