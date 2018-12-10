e3eb0a19bd008444f4e1f70f218b2fda0774191d
Dola Ram <42748067+dolaram@users.noreply.github.com>
2018-12-10 19:11:55 +0530

    Update and rename Pong_small.py to Deep_Q_cnn.py

--- CNN_deep_q_Local/Deep_Q_cnn.py
+++ CNN_deep_q_Local/Deep_Q_cnn.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 import tensorflow as tf
 import cv2
 import sys
@@ -12,68 +8,14 @@ import random
 import time 
 import numpy as np
 from collections import deque
-
-
-# In[2]:
-
-
-GAME = 'pong'
-ACTIONS = 6
-GAMMA = 0.99
-OBSERVE = 500.
-EXPLORE = 500.
-FINAL_EPSILON = 0.05
-INITIAL_EPSILON = 1.0
-REPLAY_MEMORY = 50000
-BATCH = 32
-K = 1
-
-
-# In[ ]:
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
-    return tf.Variable(initial)
-
-def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
-
-def createNetwork():
-    W_conv1 = weight_variable([8, 8, 4, 32])
-    b_conv1 = bias_variable([32])
-    W_conv2 = weight_variable([4, 4, 32, 64])
-    b_conv2 = bias_variable([64])
-    W_conv3 = weight_variable([3, 3, 64, 64])
-    b_conv3 = bias_variable([64])
-    W_fc1 = weight_variable([1600, 512])
-    b_fc1 = bias_variable([512])
-    W_fc2 = weight_variable([512, ACTIONS])
-    b_fc2 = bias_variable([ACTIONS])
-    s = tf.placeholder("float", [None, 80, 80, 4])
-    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
-    readout = tf.matmul(h_fc1, W_fc2) + b_fc2
-    return s, readout, h_fc1
-
-
-# In[3]:
-
+actions = 6
+gamma = 0.99
+replay_memory = 50000
+batch = 32
 
 def trainNetwork(s, readout, h_fc1, sess):
     tick = time.time()
-    a = tf.placeholder("float", [None, ACTIONS])
+    a = tf.placeholder("float", [None, actions])
     y = tf.placeholder("float", [None])
     readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices = 1)
     cost = tf.reduce_mean(tf.square(y - readout_action))
@@ -88,13 +30,8 @@ def trainNetwork(s, readout, h_fc1, sess):
     win_score.append(0)
     # store the previous observations in replay memory
     D = deque()
-
-    # printing
-#     a_file = open("logs_" + GAME + "/readout.txt", 'w')
-#     h_file = open("logs_" + GAME + "/hidden.txt", 'w')
-
     # get the first state by doing nothing and preprocess the image to 80x80x4
-    do_nothing = np.zeros(ACTIONS)
+    do_nothing = np.zeros(actions)
     do_nothing[0] = 1
     x_t, r_0, terminal, bar1_score, bar2_score = game_state.frame_step(do_nothing)
     x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
@@ -104,28 +41,26 @@ def trainNetwork(s, readout, h_fc1, sess):
     # saving and loading networks
     saver = tf.train.Saver()
     sess.run(tf.initialize_all_variables())
-#     checkpoint = tf.train.get_checkpoint_state("saved_networks")
-#     if checkpoint and checkpoint.model_checkpoint_path:
-#         saver.restore(sess, checkpoint.model_checkpoint_path)
-#         print( "Successfully loaded:", checkpoint.model_checkpoint_path)
-#     else:
-#         print ("Could not find old network weights")
-
+    observe = 500.
+    explore = 500.
+    FINAL_EPSILON = 0.05
+    INITIAL_EPSILON = 1.0
     epsilon = INITIAL_EPSILON
     t = 0
+    K = 1
     while True:
         readout_t = readout.eval(feed_dict = {s : [s_t]})[0]
-        a_t = np.zeros([ACTIONS])
+        a_t = np.zeros([actions])
         action_index = 0
-        if random.random() <= epsilon or t <= OBSERVE:
-            action_index = random.randrange(ACTIONS)
+        if random.random() <= epsilon or t <= observe:
+            action_index = random.randrange(actions)
             a_t[action_index] = 1
         else:
             action_index = np.argmax(readout_t)
             a_t[action_index] = 1
 
-        if epsilon > FINAL_EPSILON and t > OBSERVE:
-            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+        if epsilon > FINAL_EPSILON and t > observe:
+            epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / explore
 
         for i in range(0, K):
             x_t1_col, r_t, terminal, bar1_score, bar2_score = game_state.frame_step(a_t)
@@ -135,11 +70,11 @@ def trainNetwork(s, readout, h_fc1, sess):
             s_t1 = np.append(x_t1, s_t[:,:,0:3], axis = 2)
 
             D.append((s_t, a_t, r_t, s_t1, terminal))
-            if len(D) > REPLAY_MEMORY:
+            if len(D) > replay_memory:
                 D.popleft()
         
-        if t > OBSERVE:
-            minibatch = random.sample(D, BATCH)
+        if t > observe:
+            minibatch = random.sample(D, batch)
 
             s_j_batch = [d[0] for d in minibatch]
             a_batch = [d[1] for d in minibatch]
@@ -152,7 +87,7 @@ def trainNetwork(s, readout, h_fc1, sess):
                 if minibatch[i][4]:
                     y_batch.append(r_batch[i])
                 else:
-                    y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                    y_batch.append(r_batch[i] + gamma * np.max(readout_j1_batch[i]))
 
             train_step.run(feed_dict = {
                 y : y_batch,
@@ -161,43 +96,18 @@ def trainNetwork(s, readout, h_fc1, sess):
 
         s_t = s_t1
         t += 1
-
-        state = ""
-        if t <= OBSERVE:
-            state = "observe"
-        elif t > OBSERVE and t <= OBSERVE + EXPLORE:
-            state = "explore"
-        else:
-            state = "train"
         if r_t!= 0:
             print ("Timestep", t," Score", bar1_score)
-        
         
         win_score.pop(0)
         win_score.append(bar1_score - bar2_score)
         if(np.matrix(win_score).sum() > 72): #72
             print("Game_Ends_in Time:",int(time.time() - tick))
             break;   
-        # write info to files
-        '''
-        if t % 10000 <= 100:
-            a_file.write(",".join([str(x) for x in readout_t]) + '\n')
-            h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
-            cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
-        '''
-
-
-# In[4]:
-
-
 def playGame():
     sess = tf.InteractiveSession()
     s, readout, h_fc1 = createNetwork()
     trainNetwork(s, readout, h_fc1, sess)
-
-
-# In[ ]:
-
 
 def main():
     playGame()
@@ -208,20 +118,6 @@ if __name__ == "__main__":
     print("Game_Ends_in Time:",int(time.time() - tick))
     print("____________ END HERE _____________")
 
-
-# In[6]:
-
-
-32265/60
-
-
-# In[3]:
-
-
-print("Game_Ends_in Time:",int(time.time() - tick))
-
-
-# In[ ]:
 
 
 
