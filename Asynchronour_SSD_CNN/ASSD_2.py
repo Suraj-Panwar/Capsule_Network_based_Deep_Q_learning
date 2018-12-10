5bbe14aab3a36314524fb61025725da5e0ade0d0
Dola Ram <42748067+dolaram@users.noreply.github.com>
2018-12-10 18:29:39 +0530

    Update ASSD_2.py

--- Asynchronour_SSD_CNN/ASSD_2.py
+++ Asynchronour_SSD_CNN/ASSD_2.py
@@ -2,91 +2,14 @@ import argparse
 import sys
 import cv2
 import time
-sys.path.append("Wrapped Game Code/")
-import pong_fun as game # whichever is imported "as game" will be used
-#import tetris_fun as game
+import pong_fun as game
 import random
 import numpy as np
 from collections import deque
 import pandas as pd
 import tensorflow as tf
-
-#####################################################################################################
-
-# Create Local Game Variables
-GAME = 'pong' # the name of the game being played for log files
-ACTIONS = 6 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 500. # timesteps to observe before training
-EXPLORE = 500. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.05 # final value of epsilon
-INITIAL_EPSILON = 1.0 # starting value of epsilon
-REPLAY_MEMORY = 25000 # number of previous transitions to remember
-BATCH = 32 # size of minibatch
-K = 1 # only select an action every Kth frame, repeat prev for others
-
+from conv_fun import *
 FLAGS = None
-
-#####################################################################################################
-
-# Define layers and make the network configuration
-
-def weight_variable(shape):
-    initial = tf.random_normal(shape, stddev = 0.01)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
-    return tf.Variable(initial)
-
-def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
-
-def createNetwork():
-    # network weights
-    W_conv1 = weight_variable([8, 8, 4, 32])
-    b_conv1 = bias_variable([32])
-
-    W_conv2 = weight_variable([4, 4, 32, 64])
-    b_conv2 = bias_variable([64])
-
-    W_conv3 = weight_variable([3, 3, 64, 64])
-    b_conv3 = bias_variable([64])
-    
-    W_fc1 = weight_variable([1600, 512])
-    b_fc1 = bias_variable([512])
-
-    W_fc2 = weight_variable([512, ACTIONS])
-    b_fc2 = bias_variable([ACTIONS])
-
-    # input layer
-    s = tf.placeholder("float", [None, 80, 80, 4])
-
-    # hidden layers
-    h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
-
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
-
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
-    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
-
-    h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
-
-    # readout layer
-    readout = tf.matmul(h_fc1, W_fc2) + b_fc2
-
-    return s, readout, h_fc1
-
-#####################################################################################################
-
 
 def main(_):
   ps_hosts = FLAGS.ps_hosts.split(",")
@@ -112,8 +35,12 @@ def main(_):
 #####################################################################################################
         #sess = tf.InteractiveSession()
         print('Checkpoint 1 reached')
+        actions = 6
+        gamma = 0.99 
+        batch = 32
+        K = 1 
         s, readout, h_fc1 = createNetwork()
-        a = tf.placeholder("float", [None, ACTIONS])
+        a = tf.placeholder("float", [None, actions])
         y = tf.placeholder("float", [None])
         readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices = 1)
         cost = tf.reduce_mean(tf.square(y - readout_action))
@@ -123,7 +50,7 @@ def main(_):
         game_state = game.GameState()
 
         D =  deque()
-        do_nothing = np.zeros(ACTIONS)
+        do_nothing = np.zeros(actions)
         do_nothing[0] = 1
         x_t, r_0, terminal, bar1_score, bar2_score = game_state.frame_step(do_nothing)
         x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
@@ -134,7 +61,11 @@ def main(_):
         #var_list = tf.contrib.framework.list_variables(config.pre_model_dir)
         #for v in var_list:
             #print(v)
-
+        OBSERVE = 500. 
+        EXPLORE = 500.
+        FINAL_EPSILON = 0.05 
+        INITIAL_EPSILON = 1.0 
+        REPLAY_MEMORY = 25000 
         epsilon = INITIAL_EPSILON
         t = 0
         dct = {}
@@ -165,11 +96,11 @@ def main(_):
                 # choose an action epsilon greedily
                 #print('Checkpoint 3 reached')
                 readout_t = readout.eval(feed_dict = {s : [s_t]}, session=mon_sess)[0]
-                a_t = np.zeros([ACTIONS])
+                a_t = np.zeros([actions])
                 action_index = 0
 
                 if random.random() <= epsilon or t <= OBSERVE:
-                        action_index = random.randrange(ACTIONS)
+                        action_index = random.randrange(actions)
                         a_t[action_index] = 1
                 else:
                         action_index = np.argmax(readout_t)
@@ -210,7 +141,7 @@ def main(_):
                         if minibatch[i][4]:
                             y_batch.append(r_batch[i])
                         else:
-                            y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
+                            y_batch.append(r_batch[i] + gamma * np.max(readout_j1_batch[i]))
                     train_op.run(feed_dict = {y : y_batch,a : a_batch,s : s_j_batch}, session=mon_sess)
                     if(t%1000 == 0):
                         q_values.append(np.max(readout_t))
